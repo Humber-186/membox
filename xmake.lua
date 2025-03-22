1decf355a23a0bbb9e8b052b2040494314fc23fa
Yuhan Wang <1048429491@qq.com>
2025-03-22 08:44:04 +0800

    split xmake target into static-lib,binary

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,22 @@
 add_rules("mode.debug", "mode.release")
+add_requires("spdlog")
 
-target("membox")
+target("SV32")
+    set_kind("static")
+    add_languages("c++20")
+    add_files("src/sv32_basic.cpp", "src/sv32_supervisor.cpp", "src/buddy.cpp")
+    add_links("spdlog", "fmt")
+
+target("SV39")
+    set_kind("static")
+    add_languages("c++20")
+    add_files("src/sv39_basic.cpp", "src/sv39_supervisor.cpp", "src/buddy.cpp")
+    add_links("spdlog", "fmt")
+
+target("membox-test")
     set_kind("binary")
     add_languages("c++20")
-    add_files("src/*.cpp")
-    add_links("fmt")
+    add_deps("SV32", "SV39")
+    add_files("src/main.cpp")
+    add_links("spdlog", "fmt")
+
